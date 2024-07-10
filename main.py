ce01d5d85c31b37a96fe5b4cc6a32fdf1f6b5dbc
unknown <moumenisbest@gmail.com>
2024-07-10 18:15:29 +0200

    Added remove endpoint

--- main.py
+++ main.py
@@ -56,5 +56,21 @@ def get_key():
             cur.execute(f"SELECT key_value FROM keys WHERE key_name={data["name"]}")
             rows = cur.fetchall()
             return jsonify({"status": 200, "message": rows})
+@app.route("/remove-key", methods=["POST"])
+def remove_key():
+    data = request.get_json()
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
+    if dev_env == "prod":
+        if data["token"] != token:
+            return jsonify({"status": 400, "message":"unauthorized"})
+        else:
+            mig_funcs.remove_row(data["name"], cur)
+            conn.commit()
+            return jsonify({"status":200, "message": "OK"})
+    else:
+        mig_funcs.remove_row(data["name"], cur)
+        conn.commit()
+        return jsonify[{"status": 200, "message": "OK"}]
                     
 app.run(host="192.168.8.100", port=8080, debug=True)
--- migrations/mig_funcs.py
+++ migrations/mig_funcs.py
@@ -2,5 +2,7 @@ import sqlite3
 
 def add_row(row_data: list, cur: sqlite3.Cursor):
     cur.execute('''INSERT INTO keys(key_name, key_value) VALUES (?, ?);''', row_data)
-    
+
+def remove_row(row_name: str, cur: sqlite3.Cursor):
+    cur.execute(f'''DELETE FROM keys WHERE key_name=\"{row_name}\";''')
         
