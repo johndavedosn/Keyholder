806cadb83fc19bc43cbdd713e92992f63402b5ce
unknown <moumenisbest@gmail.com>
2024-07-10 20:53:13 +0200

    Added Component Manager

--- Component_manager/Component_manager.py
+++ Component_manager/Component_manager.py
@@ -1,5 +1,5 @@
 import libs.Paramify as Paramify
-config = Paramify.ConfigFile("./component_config.json" if __name__ == "__main__" else "./Component_manager/component_config.json")
+config = Paramify.ConfigFile("./Config/component_config.json")
 components = config.load_param("components", "")
 for component in components:
     name = component["name"]
