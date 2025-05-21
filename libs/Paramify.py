ca4643f83ca79d0c23e482bc36515bb44a1d684a
Ali moumen <moumenisbest@gmail.com>
2025-05-21 21:53:51 +0200

    added some improvement to Paramify

--- libs/Paramify.py
+++ libs/Paramify.py
@@ -1,59 +1,46 @@
 import json
 from time import sleep
-class LogicalParam:
-    def __init__(self, param):
-        self.param = param
-
-    def bind(self, func):
-        if self.param:
-            func()
-    def get_value(self):
-        return self.param
-
 class ConfigFile:
-    def __init__(self, file):
-        self.file = file
-        if ".json" not in file:
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        if ".json" not in file_name:
             raise ValueError("Sorry, but this module only supports json for now")
-        with open(file, "r") as config_file:
+        with open(file_name, "r") as config_file:
             config = json.load(config_file)
-        self.config = config
+        self.config: dict = config
 
-    def load_param(self, param, type, default=None):
+    def load_param(self, param: str, default: str | None = None):
         if param in self.config:
             param_loaded = self.config[param]
-            if type == "log":
-                return LogicalParam(param_loaded)
-            else:
-                return param_loaded
+            return param_loaded
         else:
             return default
     
     def list_parameters(self):
         return list(self.config.keys())
     
-    def is_in_config(self, param):
+    def is_in_config(self, param: str):
         return param in self.config
     
-    def update_param(self, param, value):
+    def update_param(self, param: str, value):
         if param in self.config:
             self.config[param] = value
             with open(self.file, "w") as config_file:
                 json.dump(self.config, config_file, indent=2)
-    def validate(self, param_name, expected_type):
+    def validate(self, param_name: str, expected_type):
         if param_name in self.config:
             param_value = self.config[param_name]
             if isinstance(param_value, expected_type):
                 return True
         return False
 class ConfigStream:
-    def __init__(self, config_file):
-        self.config_file = config_file
+    def __init__(self, config_filename: str):
+        self.config_file = config_filename
 
-    def update_param(self, param, value):
+    def update_param(self, param: str, value):
         config_file = ConfigFile(self.config_file)
         config_file.update_param(param, value)
-    def stream(self, param ,values, delay=0):
+    def stream(self, param ,values: list, delay=0):
         config_file = ConfigFile(self.config_file)
         for value in values:
             config_file.update_param(param, value)
