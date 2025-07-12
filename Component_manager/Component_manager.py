f0cfd935965aac796c5eb162d433ba52d08b0115
Ali moumen <moumenisbest@gmail.com>
2025-07-12 22:13:46 +0200

    Slight changes to the component manager

--- Component_manager/Component_manager.py
+++ Component_manager/Component_manager.py
@@ -1,8 +1,13 @@
 import libs.Paramify as Paramify
+import subprocess
+import os
 config = Paramify.ConfigFile("component_config.json")
-components = config.load_param("components", "")
+components: list[dict] = config.load_param("components", "")
 for component in components:
     name = component["name"]
     enabled = component["enabled"]
     if enabled:
-        exec(f"import components.{name}")
+        if os.name == "nt":
+            subprocess.run(["py", name])
+        if os.name == "posix":
+            subprocess.run(["python3", name])    
